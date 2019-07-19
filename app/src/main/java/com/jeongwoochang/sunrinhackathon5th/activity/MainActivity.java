c47b95ef4631934b33352c4288afd4544773a2f3
Clouds <swliris24@gmail.com>
2019-07-20 02:23:31 +0900

    Main Complete

--- app/src/main/java/com/jeongwoochang/sunrinhackathon5th/activity/MainActivity.java
+++ app/src/main/java/com/jeongwoochang/sunrinhackathon5th/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package com.jeongwoochang.sunrinhackathon5th.activity;
 
 import android.app.Application;
+import android.content.Intent;
+import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.cardview.widget.CardView;
 import com.jeongwoochang.sunrinhackathon5th.MyApplication;
 import com.jeongwoochang.sunrinhackathon5th.R;
 import com.jeongwoochang.sunrinhackathon5th.util.SharedPreferencesHelper;
@@ -12,6 +15,7 @@ import org.joda.time.DateTime;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView writeDiaryBtn, myDiaryBtn, yourDiaryBtn;
     TextView mainDate;
 
     @Override
@@ -30,11 +34,38 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mainDate = findViewById(R.id.main_date);
+        writeDiaryBtn = findViewById(R.id.main_btn_diary_write);
+        myDiaryBtn = findViewById(R.id.main_btn_diary_my);
+        yourDiaryBtn = findViewById(R.id.main_btn_diary_your);
 
         DateTime dateTime = new DateTime();
         String today = dateTime.toString("오늘은 yyyy년 MM월 dd일 입니다.");
 
         mainDate.setText(today);
+
+        writeDiaryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, WriteDiaryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        myDiaryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MyDiaryListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        yourDiaryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, YourDiaryListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
