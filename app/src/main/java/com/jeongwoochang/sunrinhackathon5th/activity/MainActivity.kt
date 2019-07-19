69bcb8c4455746ea2bd1e1d9e00d7944d3c217a8
Clouds <swliris24@gmail.com>
2019-07-19 23:10:35 +0900

    dasi

--- app/src/main/java/com/jeongwoochang/sunrinhackathon5th/activity/MainActivity.kt
+++ app/src/main/java/com/jeongwoochang/sunrinhackathon5th/activity/MainActivity.kt
@@ -15,30 +15,9 @@ import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var drawerToggle: ActionBarDrawerToggle? = null
-    var drawer_str = arrayOf("mypage", "setup", "help")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolBar)
-
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, drawer_str)
-        drawer.adapter = adapter
-
-        drawerToggle = object : ActionBarDrawerToggle(this, drawerlayout, R.string.open, R.string.close) {
-            override fun onDrawerOpened(drawerView: View) {
-                super.onDrawerOpened(drawerView)
-            }
-
-            override fun onDrawerClosed(drawerView: View) {
-                super.onDrawerClosed(drawerView)
-            }
-        }
-        drawerlayout.addDrawerListener(drawerToggle as ActionBarDrawerToggle)
-
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        supportActionBar!!.setHomeButtonEnabled(true)
 
         MyApplication.getInstance().setOnVisibilityChangeListener {
             if (it) {
@@ -46,19 +25,5 @@ class MainActivity : AppCompatActivity() {
                     SharedPreferencesHelper(this).removeCookies()
             }
         }
-    }
-
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-        drawerToggle!!.syncState()
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        drawerToggle!!.onConfigurationChanged(newConfig)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return if (drawerToggle!!.onOptionsItemSelected(item)) true else super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/jeongwoochang/sunrinhackathon5th/activity/MyDiaryActivity.java
@@ -0,0 +1,14 @@
+package com.jeongwoochang.sunrinhackathon5th.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.jeongwoochang.sunrinhackathon5th.R;
+
+public class MyDiaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_diary);
+    }
+}
